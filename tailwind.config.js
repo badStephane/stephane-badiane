cf00bc4438a1540b2d395759cdc29490f474a8c8
sbadiane <badstephane14@gmail.com>
2024-09-26 00:26:46 +0000

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily:{
+      'sans':['Inter', 'sans-serif']
+    }
+  },
+};
+export const plugins = [];
